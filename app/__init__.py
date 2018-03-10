68995cd0dff300269bf3b0c1178222b719fcc765
wafa-b <wafa_609@hotmail.com>
2018-03-10 14:15:28 +0300

    Fix __init__ file

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,10 @@
-from flask import Flask, render_template
-import models
-import stores
-import dummy_data
-from views import *
+from flask import Flask
+from app import models,stores,dummy_data
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/index")
-def home():
-    return render_template("index.html", posts = post_store.get_all())
-
-
 member_store = stores.MemberStore()
 post_store = stores.PostStore()
+dummy_data.seed_stores(member_store, post_store)
 
-if __name__  == "__main__":
-    dummy_data.seed_stores(member_store, post_store)
-    app.run()
+from app import views
