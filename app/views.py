24ddd82524b021113e57def759648dede2ef3025
wafa-b <wafa_609@hotmail.com>
2018-03-13 19:20:22 +0300

    Add CRUD actions

--- app/views.py
+++ app/views.py
@@ -16,3 +16,26 @@ def topic_add():
 
     else:
         return render_template("topic_add.html")
+
+@app.route("/topic/topic_show/<int:id>")
+def topic_show(id):
+    post = post_store.get_by_id(id)
+    return render_template("topic_show.html", post = post)
+
+@app.route("/topic/topic_update/<int:id>", methods = ["GET", "POST"])
+def topic_update(id):
+    post = post_store.get_by_id(id)
+    if request.method == "POST":
+        post.title = request.form["title"]
+        post.content = request.form["content"]
+        post_store.update(post)
+        return redirect(url_for("home"))
+
+    elif request.method == "GET":
+        return render_template("topic_update.html", post = post)
+
+
+@app.route("/topic/delete/<int:id>")
+def topic_delete(id):
+    post_store.delete(id)
+    return redirect(url_for("home"))
